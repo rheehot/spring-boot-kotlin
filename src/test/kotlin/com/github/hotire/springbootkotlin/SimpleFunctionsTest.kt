4dde8f96b80250da4f1c3b6a085253e6a0f72ae1
user <gngh0101@gmail.com>
2019-12-15 21:07:24 +0900

    Verify default and named in function

--- src/test/kotlin/com/github/hotire/springbootkotlin/SimpleFunctionsTest.kt
+++ src/test/kotlin/com/github/hotire/springbootkotlin/SimpleFunctionsTest.kt
@@ -2,11 +2,7 @@ package com.github.hotire.springbootkotlin
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-
-/**
- * @author : hotire
- */
-
+import java.util.*
 
 class SimpleFunctionsTest {
     /**
@@ -17,6 +13,18 @@ class SimpleFunctionsTest {
     @Test
     fun test_start() {
         assertThat(start()).isEqualTo("OK")
+    }
+
+    /**
+     * It can be called on a collection of Strings. Specifying only two arguments make the function joinOptions()
+     * return the list in a JSON format (e.g., "[a, b, c]")
+     */
+    private fun joinOptions(options: Collection<String>): String =
+            options.joinToString(prefix = "[", postfix = "]")
+
+    @Test
+    fun test_joinOptions() {
+        assertThat(joinOptions(Arrays.asList("a", "b", "c"))).isEqualTo("[a, b, c]")
     }
 
 }
