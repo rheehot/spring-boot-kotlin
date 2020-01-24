088be3bbc3bf5482502d367e87b4ba0597c4b67c
hotire <gngh0101@gmail.com>
2020-01-25 00:12:38 +0900

    Lean custom function

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex03/Custom.kt
@@ -0,0 +1,18 @@
+package com.github.hotire.springbootkotlin.books.in_action.ex03
+
+import java.lang.StringBuilder
+
+
+fun<T> joinToString (collection: Collection<T>, separator: String, prefix: String, postfix: String) : String {
+    val result = StringBuilder(prefix)
+    for ((index, el) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(el)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    print(joinToString(listOf(1, 2, 3), ":", "(", ")"))
+}
