cea78282ab94fcc96c18acd56c995dbecbcfdcbf
hotire <gngh0101@gmail.com>
2020-01-24 15:58:19 +0900

    Learn when

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex02/Color.kt
@@ -0,0 +1,18 @@
+package com.github.hotire.springbootkotlin.books.in_action.ex02
+
+import java.awt.Color.RED
+
+enum class Color {
+    RED, ORANGE, BLUE
+}
+
+fun main(args: Array<String>) {
+    print(convert(Color.BLUE))
+}
+
+fun convert(color: Color) : String =
+        when(color) {
+            Color.RED -> "1"
+            Color.ORANGE -> "2"
+            Color.BLUE -> "3"
+        }
