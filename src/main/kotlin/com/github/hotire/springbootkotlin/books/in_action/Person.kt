c20f63f4e4ca4b4c74113621662560f7285ebe21
hotire <gngh0101@gmail.com>
2020-01-24 14:38:01 +0900

    Add example of kotlin in action

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/Person.kt
@@ -0,0 +1,12 @@
+package com.github.hotire.springbootkotlin.books.in_action
+
+data class Person(val name: String, val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person("tire"), Person("ho", age = 29))
+
+    val oldest = persons.maxBy { it.age ?: 0 }
+    print("max : $oldest")
+
+}
+
