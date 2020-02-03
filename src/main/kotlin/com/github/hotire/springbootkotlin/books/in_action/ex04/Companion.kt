14e14bee0e24979b828fd6e7dc1febae2ec3aa15
hotire <gngh0101@gmail.com>
2020-02-04 00:37:48 +0900

    Learn how to implements of companion

--- src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex04/Companion.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex04/Companion.kt
@@ -17,7 +17,21 @@ class B {
     }
 }
 
+
+interface JSONFactory<T> {
+    fun fromJSON(text: String) : T
+}
+
+class C {
+    companion object : JSONFactory<C> {
+        override fun fromJSON(text: String): C {
+            return C()
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     A.bar();
     B.Name.naming()
+    C.fromJSON("")
 }
