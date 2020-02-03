65bec71153e856594ca4078494391efc2207a4b1
hotire <gngh0101@gmail.com>
2020-02-04 00:30:15 +0900

    Learn how to use companion

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex04/Companion.kt
@@ -0,0 +1,14 @@
+package com.github.hotire.springbootkotlin.books.in_action.ex04
+
+
+class A {
+    companion object {
+        fun bar() {
+
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    A.bar();
+}
