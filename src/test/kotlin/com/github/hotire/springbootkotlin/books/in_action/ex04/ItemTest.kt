0a14cd407dc8c331e2547e9e0724757c61f7a42e
hotire <gngh0101@gmail.com>
2020-02-27 00:45:27 +0900

    Verify setter

--- src/test/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex04/ItemTest.kt
+++ src/test/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex04/ItemTest.kt
@@ -1,5 +1,20 @@
 package com.github.hotire.springbootkotlin.books.in_action.ex04
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-internal class ItemTest
+internal class ItemTest {
+
+    @Test
+    fun alias() {
+        // given
+        val expected = "test"
+        val item = Item("")
+
+        // when
+        item.alias = expected
+
+        // then
+        assertThat(item.alias).isEqualTo(expected);
+    }
+}
