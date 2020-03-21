029a959ce96eab20b02d36c0d596b76f685c6bc8
hotire <gngh0101@gmail.com>
2020-03-21 22:20:35 +0900

    Add example code of reflection kotlin

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/Reflection.kt
@@ -0,0 +1,23 @@
+package com.github.hotire.springbootkotlin.books.in_action.ex10
+
+import kotlin.reflect.KAnnotatedElement
+import kotlin.reflect.full.findAnnotation
+
+class Reflection(val name: String)
+
+fun main() {
+    val reflection = Reflection("")
+    val c = Reflection::class.java.kotlin;
+    reflection.javaClass.kotlin;
+
+
+
+    val custom = Reflection::class.findAnnotation<CustomSerializer>();
+    Reflection::class.java.declaredFields
+}
+
+
+
+//fun <T : Annotation> KAnnotatedElement.findAnnotation2(): T? =
+//        @Suppress("UNCHECKED_CAST")
+//        annotations.firstOrNull { it is T } as T?
