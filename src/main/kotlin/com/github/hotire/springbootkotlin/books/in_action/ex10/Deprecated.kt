a990489afcf5bf633b54034fdf094b31cc414b38
hotire <gngh0101@gmail.com>
2020-03-21 14:41:25 +0900

    Learn how to use Deprecated annoation

---
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/Deprecated.kt
@@ -0,0 +1,8 @@
+package com.github.hotire.springbootkotlin.books.in_action.ex10
+
+
+@Deprecated("")
+class Functions {
+    @Deprecated("Use println() instead", ReplaceWith("println()"))
+    fun enter() = println()
+}
