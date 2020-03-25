b972c6f6f843d0d9e472c4134a9547d5207a7a64
hotire <gngh0101@gmail.com>
2020-03-26 02:22:30 +0900

    Implement HashCode

--- src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/AnnoationAndReflection.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/AnnoationAndReflection.kt
@@ -1,8 +1,14 @@
 package com.github.hotire.springbootkotlin.books.in_action.ex10
 
 import org.springframework.core.annotation.Order
-import kotlin.reflect.full.memberProperties
+import java.lang.annotation.Inherited
+import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.superclasses
+import kotlin.reflect.KProperty as KProperty1
 
+@ExperimentalStdlibApi
 fun Any.getHash() : String  {
     if (this is HashAware) {
         return getHash()
@@ -10,23 +16,46 @@ fun Any.getHash() : String  {
     return toString()
 }
 
+fun KClass<*>.declaredMemberProperty(propertyName: String) : kotlin.reflect.KProperty1<out Any, Any?>? {
+    val s =  this.declaredMemberProperties.firstOrNull() { it.name == propertyName }
+    return s
+}
+
+
 interface HashAware {
+    @Inherited
+    @Retention
     @Target(AnnotationTarget.PROPERTY)
     annotation class HashCode(val order: Order = Order())
 
-    fun getHash() : String {
-        this::class.memberProperties.forEach{
-            val result = it.call(this)
-            println(result?.getHash())
-        }
-        return ""
+    fun <R> findAnnotation(kProperty: KProperty1<R>) : HashCode? {
+        return kProperty.findAnnotation<HashCode>() ?: this::class.superclasses.map {
+            it.declaredMemberProperty(kProperty.name)?.findAnnotation<HashCode>();
+        }.firstOrNull()
     }
+
+    fun <R> hasAnnotation(kProperty: KProperty1<R>): Boolean = findAnnotation(kProperty) != null
+
+    @ExperimentalStdlibApi
+    fun getHash() : String {
+        return this::class.declaredMemberProperties
+                .asSequence()
+                .map { it -> print(it.annotations)
+                    println(findAnnotation(it))
+                    it
+                }
+                .filter { hasAnnotation(it)}
+                .map { getPrefix() + findAnnotation(it)!!.order.value + it.call(this) }
+                .joinToString();
+    }
+
+    fun getPrefix() : String = this::class.simpleName ?: ""
 }
 
 interface PersonHashAware : HashAware {
     @HashAware.HashCode(Order(1))
     val name : String
-    @HashAware.HashCode(Order(1))
+    @HashAware.HashCode(Order(2))
     val age: Int
 }
 
@@ -39,9 +68,11 @@ class Person2(override val name: String, override val age: Int) : PersonHashAwar
     }
 }
 
+@ExperimentalStdlibApi
 fun main() {
     val p = Person2("hello", 1)
     val hash = p.getHash();
+    print(hash)
 }
 
 
