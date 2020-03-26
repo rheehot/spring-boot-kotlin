dba11f6781f54bb088bc64bcf1a54cf116b0a1d3
hotire <gngh0101@gmail.com>
2020-03-27 00:42:43 +0900

    Move findAnnotation to extension

--- src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/ReflectionUtils.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/ReflectionUtils.kt
@@ -9,8 +9,10 @@ import kotlin.reflect.full.superclasses
 fun KClass<*>.declaredMemberProperty(propertyName: String) : kotlin.reflect.KProperty1<out Any, Any?>?
      = this.declaredMemberProperties.firstOrNull() { it.name == propertyName }
 
-inline fun <reified T : Annotation, R> findAnnotation(obj: Any, kProperty: KProperty<R>) : T? {
-    return kProperty.findAnnotation<T>() ?: obj::class.superclasses
+
+inline fun <reified T : Annotation, R> Any.findAnnotation(kProperty: KProperty<R>) : T? {
+    return kProperty.findAnnotation<T>() ?: this::class.superclasses
             .map { it.declaredMemberProperty(kProperty.name)?.findAnnotation<T>() }
             .firstOrNull()
 }
+
