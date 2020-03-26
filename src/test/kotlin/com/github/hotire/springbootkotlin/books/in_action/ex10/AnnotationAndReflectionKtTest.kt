2da0f0831265320afff0a9178430144755e95c17
hotire <gngh0101@gmail.com>
2020-03-27 01:09:17 +0900

    Verify getHash

--- src/test/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/AnnotationAndReflectionKtTest.kt
+++ src/test/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex10/AnnotationAndReflectionKtTest.kt
@@ -1,12 +1,18 @@
 package com.github.hotire.springbootkotlin.books.in_action.ex10
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class AnnoationAndReflectionKtTest {
-
+internal class AnnotationAndReflectionKtTest {
     @Test
     fun getHash() {
+        // given
+        val person = PersonEntity("hello", 2)
 
+        // when
+        val result = person.getHash()
+
+        // then
+        assertThat(result).isEqualTo("PersonEntity1hello, PersonEntity22");
     }
 }
