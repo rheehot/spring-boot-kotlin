b0fe9996fff5f6a4e2c29fc3fd9fb989a40775d6
hotire <gngh0101@gmail.com>
2020-03-30 00:09:53 +0900

    Make compile() be added dependency to dependencies

--- src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex11/Invoke.kt
+++ src/main/kotlin/com/github/hotire/springbootkotlin/books/in_action/ex11/Invoke.kt
@@ -2,9 +2,10 @@ package com.github.hotire.springbootkotlin.books.in_action.ex11
 
 
 class DependencyHandler {
-    val dependencies = listOf<String>()
+    val dependencies = mutableListOf<String>()
 
     fun compile(coordinate: String) {
+        dependencies.add(coordinate)
         println("Add dependency on ${coordinate}")
     }
 
